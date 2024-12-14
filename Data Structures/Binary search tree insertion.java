9fc1554f5c023c96d466c7eee413eebdade7afc7
Balla Kavya <ballakavya594@gmail.com>
2024-12-14 21:06:26 +0530

    Binary Search Tree : Insertion

---
+++ Data Structures/Binary search tree insertion.java
@@ -0,0 +1,13 @@
+public static Node insert(Node root, int data) {
+    if (root == null) {
+        return new Node(data);
+    }
+    if (data < root.data) {
+        root.left = insert(root.left, data);
+    }
+    if (data > root.data) {
+        root.right = insert(root.right, data);
+    }
+    return root;
+
+}
